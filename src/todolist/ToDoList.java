f4e240a62bf0ff69c8ab1b06b0df1b006cf47029
Artem <Artem@192.168.1.4>
2017-08-27 13:38:01 +0300

    refactoring

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
 
-
 /**
  *
  * @author Artem
@@ -25,83 +24,62 @@ public class ToDoList {
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
+     * @throws java.lang.ClassNotFoundException
      */
-    	public static void main (String [] args) throws IOException, ClassNotFoundException {
-            List<ToDoListItem> todoList = new ArrayList<>();                                  
-                      
-            ArrayList<String> things = new ArrayList<>();
-            File file = new File("list.txt");
-            FileWriter writer = new FileWriter(file, true);
-            //readFileToArrayList(file, things);
-            
-            FileInputStream fileInStream = new FileInputStream(file);
-            ObjectInputStream objectInStream = new ObjectInputStream(fileInStream);
-            todoList = (ArrayList<ToDoListItem>)objectInStream.readObject();
-            objectInStream.close();
-            
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        File file = new File("list.txt");
+        FileInputStream fileInStream = new FileInputStream(file);
+        ObjectInputStream objectInStream = new ObjectInputStream(fileInStream);
+        List<ToDoListItem> todoList = (ArrayList<ToDoListItem>) objectInStream.readObject();
+        objectInStream.close();
+        String text = "Input number (1 - add thing, 2 - show things, 3 - delete thing, 4 - exit): ";
+        int number = 0;
+        while (number != 4) {
+            System.out.println(text);
             Scanner in = new Scanner(System.in);
-            Scanner str = new Scanner(System.in);
-            Scanner bool = new Scanner(System.in);
-            String text = "Input number (1 - add thing, 2 - show things, 3 - delate thing, 4 - exit): ";
-            int number = 0;
-            String name;
-            while (number != 4) {
-                System.out.println(text);
-		number = in.nextInt();
-                switch(number){
-                    case 1:
-                        System.out.println("add thing: ");
-                        String line = str.nextLine();
-                        todoList.add(new ToDoListItem() {{
+            number = in.nextInt();
+            switch (number) {
+                case 1:
+                    System.out.println("add thing: ");
+                    Scanner str = new Scanner(System.in);
+                    String line = str.nextLine();
+                    todoList.add(new ToDoListItem() {
+                        {
                             Text = line;
-                            //Complete = false;
-                            //BeginDate = new Date(2017, 8, 25);
-                            //EndDate = new Date(2017, 8, 26);
-                        }});                       
-                        break;
-                    case 2:                        
-                        todoList.forEach((todoItem) -> {                            
-                            System.out.println(todoItem.Text);
-                        });
-                        break;
-                    case 3:
-                        System.out.println("Etnter the number of delating thing: ");
-                        number = in.nextInt();
-                        things.remove(number);
-                        number = 0;
-                        printList(things);
-                        break;
-                    case 4:                        
-                        break;
-                    default:
-                        System.out.println("It's not correct number");
-                }		
-            }
-            FileOutputStream fileOutStream = new FileOutputStream(file);
-            ObjectOutputStream objectOutStream = new ObjectOutputStream(fileOutStream);
-            objectOutStream.writeObject(todoList);
-            objectOutStream.close();                
-	}        	
-        public static ArrayList readFileToArrayList(File file, ArrayList things) throws IOException {
-		Scanner listFile = new Scanner(file);
-                things.clear();
-                while(listFile.hasNext()){
-                    things.add(listFile.next());
-                }
-		return(things);
-	}
-        public static void printList (ArrayList things) {
-            int a = 0;
-            while (a != things.size()) {
-                System.out.println(a + " - " + things.get(a));
-                a++;
+                            Complete = false;
+                        }
+                    });
+                    break;
+                case 2:
+                    printList(todoList);
+                    break;
+                case 3:
+                    System.out.println("Etnter the number of deleting thing: ");
+                    int numberItem = in.nextInt();
+                    todoList.remove(numberItem);
+                    printList(todoList);
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("It's not correct number");
             }
         }
+        FileOutputStream fileOutStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutStream = new ObjectOutputStream(fileOutStream);
+        objectOutStream.writeObject(todoList);
+        objectOutStream.close();
+    }
+    public static void printList(List<ToDoListItem> todoList) {
+        todoList.forEach((todoItem) -> {
+            System.out.println(todoItem.Text);
+        });
+    }
 }
+
 class ToDoListItem implements Serializable {
+
     public String Text;
-    //public boolean Complete;
-    //public Date BeginDate;
-    //public Date EndDate;
-    
+    public boolean Complete;
 }
