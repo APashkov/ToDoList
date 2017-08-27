a8721a0b83c3eb4c77583ab1ddf3f7ad6c064d50
Artem <Artem@192.168.1.4>
2017-08-27 13:05:34 +0300

    added readFileToList

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -26,14 +26,19 @@ public class ToDoList {
      * @param args the command line arguments
      * @throws java.io.IOException
      */
-    	public static void main (String [] args) throws IOException {
+    	public static void main (String [] args) throws IOException, ClassNotFoundException {
             List<ToDoListItem> todoList = new ArrayList<>();                                  
                       
             ArrayList<String> things = new ArrayList<>();
-            //List<List<String>> things = new ArrayList<>();
             File file = new File("list.txt");
             FileWriter writer = new FileWriter(file, true);
-            readFileToArrayList(file, things);
+            //readFileToArrayList(file, things);
+            
+            FileInputStream fileInStream = new FileInputStream(file);
+            ObjectInputStream objectInStream = new ObjectInputStream(fileInStream);
+            todoList = (ArrayList<ToDoListItem>)objectInStream.readObject();
+            objectInStream.close();
+            
             Scanner in = new Scanner(System.in);
             Scanner str = new Scanner(System.in);
             Scanner bool = new Scanner(System.in);
@@ -45,9 +50,10 @@ public class ToDoList {
 		number = in.nextInt();
                 switch(number){
                     case 1:
-                        System.out.println("add thing: ");                     
+                        System.out.println("add thing: ");
+                        String line = str.nextLine();
                         todoList.add(new ToDoListItem() {{
-                            Text = str.nextLine();
+                            Text = line;
                             //Complete = false;
                             //BeginDate = new Date(2017, 8, 25);
                             //EndDate = new Date(2017, 8, 26);
@@ -65,52 +71,17 @@ public class ToDoList {
                         number = 0;
                         printList(things);
                         break;
+                    case 4:                        
+                        break;
                     default:
                         System.out.println("It's not correct number");
                 }		
             }
-            //writeArrayListToFile(file, things);
-            //writeListToFile(file, todoList);
             FileOutputStream fileOutStream = new FileOutputStream(file);
             ObjectOutputStream objectOutStream = new ObjectOutputStream(fileOutStream);
             objectOutStream.writeObject(todoList);
-            objectOutStream.close();
-            //OutputStream outStream = new FileOutputStream(file);
-                    //outStream.write(todoList);
-                
-	}
-
-	public static void writeLine(File file, String name) throws IOException {
-		FileWriter writer = new FileWriter(file, true);
-                try (BufferedWriter bufferWriter = new BufferedWriter(writer)) {
-                    bufferWriter.write(name);
-                    bufferWriter.newLine();                    
-                }
-	}
-	public static void readFile(File file) throws IOException {
-		Scanner listFile = new Scanner(file);
-                while(listFile.hasNext()){
-                    System.out.println(listFile.next());
-                }
-	}
-        public static void writeListToFile(File file, List things) throws IOException {
-                    Iterator <String> iteratorThings = things.iterator();
-                    Formatter f = new Formatter(file);
-                    f.format("%s","");
-                    while(iteratorThings.hasNext()){
-                        String line = iteratorThings.next();
-                        writeLine(file, line);
-                    }                
-                }
-	public static void writeArrayListToFile(File file, ArrayList things) throws IOException {
-                    Iterator <String> iteratorThings = things.iterator();
-                    Formatter f = new Formatter(file);
-                    f.format("%s","");
-                    while(iteratorThings.hasNext()){
-                        String line = iteratorThings.next();
-                        writeLine(file, line);
-                    }                
-                }
+            objectOutStream.close();                
+	}        	
         public static ArrayList readFileToArrayList(File file, ArrayList things) throws IOException {
 		Scanner listFile = new Scanner(file);
                 things.clear();
