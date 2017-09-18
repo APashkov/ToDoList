8dc14ddb999c414385237a4a61af833e5381ed57
Vadim Pashkov <vad.pashkov@gmail.com>
2017-09-18 21:48:58 +0300

    Add Spring.Boot

--- src/main/java/com/pashkov/artem/todolist/ToDoList.java
+++ src/main/java/com/pashkov/artem/todolist/ToDoList.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package todolist;
+package com.pashkov.artem.todolist;
 
 import java.util.ArrayList;
 import java.util.List;
